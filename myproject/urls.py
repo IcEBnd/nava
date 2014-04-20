6691425384dcf5e3b171be73f4e5ebe81502bd5c
IcE <ice@f0bar.org>
2014-04-20 15:03:15 +0200

    added network

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^nava/(?P<channel>[a-z\.]+)$', 'nava.views.home'),
+    url(r'^nava/(?P<network>[a-z]+)/(?P<channel>[a-z\.]+)$', 'nava.views.home'),
 )
--- nava/views.py
+++ nava/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-def home(request, channel):
+def home(request, network, channel):
 	print(channel)
 
 	template = loader.get_template('index.html')
-	context = RequestContext(request, {'channel': channel})
+	context = RequestContext(request, {'network': network, 'channel': channel})
 	return HttpResponse(template.render(context))
 	#return http.HttpResponse('Hello World! This is changed')
 
