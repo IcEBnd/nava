f8f1c6d6ea296a7fab95c5984c9aa5f939416ebd
IcEBnd <icebnd@f0bar.org>
2014-04-21 22:33:40 +0200

    removed print from views

--- appengine/nava/views.py
+++ appengine/nava/views.py
@@ -2,10 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 def home(request, network, channel):
-	print(channel)
-
 	template = loader.get_template('index.html')
 	context = RequestContext(request, {'network': network, 'channel': channel})
 	return HttpResponse(template.render(context))
-	#return http.HttpResponse('Hello World! This is changed')
 
